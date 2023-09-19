ea71b964f4fcb56b4fa98268fa0b408248151920
Abdallah-Ben <abdallah_ben@me.com>
2023-09-19 15:27:17 +0100

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,10 +1,15 @@
-import express, { Router } from 'express'
-import serverless from 'serverless-http'
+import express from 'express'
 
-const api = express()
+const app = express()
+const port = process.env.PORT || 5020
 
-const router = Router()
-router.post('/calculator', (req, res) => {
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('You are in')
+})
+
+app.post('/calculator', (req, res) => {
   // Get the request body
   const body = req.body
 
@@ -38,55 +43,6 @@ router.post('/calculator', (req, res) => {
   res.send({ result })
 })
 
-api.use('/api/', router)
+// app.use('/test', testRoutes)
 
-export const handler = serverless(api)
-
-////
-
-// import express from 'express'
-
-// const app = express()
-// const port = process.env.PORT || 5020
-
-// app.use(express.json())
-
-// app.get('/', (req, res) => {
-//   res.send('You are in')
-// })
-
-// app.post('/calculator', (req, res) => {
-//   // Get the request body
-//   const body = req.body
-
-//   // Get the two numbers and the operator
-//   const num1 = body.num1
-//   const num2 = body.num2
-//   const operator = body.operator
-
-//   // Perform the calculation based on the operator
-//   let result
-//   if (typeof num1 === 'number' && typeof num2 === 'number') {
-//     switch (operator) {
-//       case '+':
-//         result = num1 + num2
-//         break
-//       case '-':
-//         result = num1 - num2
-//         break
-//       case '*':
-//         result = num1 * num2
-//         break
-//       default:
-//         result = 'Invalid operator'
-//         break
-//     }
-//   } else {
-//     result = 'Invalid numbers.'
-//   }
-
-//   // Send the response
-//   res.send({ result })
-// })
-
-// app.listen(port, () => console.log('You are in'))
+app.listen(port, () => console.log('You are in'))
